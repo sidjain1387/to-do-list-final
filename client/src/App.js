8b20992f1b280463fb4791d2f7e2f8477a2c2364
Siddhant Jain <sidjain1387@gmail.com>
2025-03-22 15:53:06 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React,{Fragment} from 'react';
+import './App.css';
+
+//Components
+import InputTodo from './components/InputTodo';
+import ListTodo from './components/ListTodo';
+
+
+function App() {
+  return (
+    <Fragment>
+      <div className='container'>
+        <InputTodo/>
+        <ListTodo/>
+      </div>
+    </Fragment>
+
+  );
+}
+
+export default App;
